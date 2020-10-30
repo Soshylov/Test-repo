485c60b1e95fa3a96b8d6fee609e678cb4b70c68
d.soshylov <d.soshylov@pampamstudio.com>
2020-10-30 10:15:37 +0200

    Тест репозитория

---
+++ Test.cs
@@ -0,0 +1,19 @@
+﻿using Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log($"Test repository");
+    }
+
+    private void Awake()
+    {
+
+       // PlayerPrefs.DeleteAll();
+    }
+
+}
