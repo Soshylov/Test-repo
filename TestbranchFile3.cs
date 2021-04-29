d54b2826c99c07225167ae8039b5e224ece254b1
D.Soshylov <d.soshylov@pampamstudio.com>
2021-04-29 13:25:43 +0300

    Revert "Revert "Merge branch 'Test_branch' into master""
    This reverts commit 6e5fd3d037c42843725f7f415f2ccf755a4656e3.

---
+++ TestbranchFile3.cs
@@ -0,0 +1,17 @@
+﻿using Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestbranchFile : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log($"Test repository");
+        Debug.Log($"Новая строка");
+
+        Debug.Log($"Еще одна строка в ветке");
+        
+    }
+
+}
---
+++ TestbranchFile4.cs
@@ -0,0 +1,15 @@
+﻿using Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestbranchFile : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log($"Новая строка в ветке");
+        Debug.Log($"Еще одна строка в ветке");
+        
+    }
+
+}
