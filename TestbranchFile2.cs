abb1d00119052dfe9d366a40ab07ab50db3756b9
Maks Uells <maks@pampamstudio.com>
2021-04-29 13:14:36 +0300

    Тест сливания ветки

---
+++ TestbranchFile2.cs
@@ -0,0 +1,18 @@
+﻿using Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestbranchFile : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log($"Test repository");
+        Debug.Log($"Новая строка");
+        Debug.Log($"Новая строка в мейне");
+        Debug.Log($"Новая строка в ветке");
+        Debug.Log($"Еще одна строка в ветке");
+        
+    }
+
+}
