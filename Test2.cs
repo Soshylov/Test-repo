e10f4bf09b21eb955234090c6f19d842d761aee4
D.Soshylov <d.soshylov@pampamstudio.com>
2021-04-29 13:22:54 +0300

    fix

---
+++ Test2.cs
@@ -0,0 +1,24 @@
+﻿using Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log($"Test repository");
+        Debug.Log($"Новая строка");
+        Debug.Log($"Новая строка в мейне");
+        Debug.Log($"Новая строка в ветке");
+        Debug.Log($"Еще одна строка в ветке");
+        
+    }
+
+    private void Awake()
+    {
+
+       // PlayerPrefs.DeleteAll();
+    }
+
+}
